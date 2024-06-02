438db23eef8cdac97849a8ecd2d094575f4a39c1
Jayden <59065556+Jdare2001@users.noreply.github.com>
2024-06-02 15:17:09 +0100

    add add item button

--- SQLCommands/TableCreation.sql
+++ SQLCommands/TableCreation.sql
@@ -1,6 +1,15 @@
-CREATE TABLE items(
-	id INT auto_increment Primary Key,
+Use IMSystem;
+
+CREATE TABLE Items(
+	Id INT auto_increment Primary Key,
     Name varchar(255) NOT NULL,
     Description TEXT,
     Quantity INT DEFAULT 0
 );
+CREATE TABLE Deliveries(
+	Id Int auto_increment Primary Key,
+    ItemID int,
+    Quantity int,
+    DeliveryDate DATE,
+    FOREIGN KEY (ItemId) REFERENCES Items(Id)
+)
